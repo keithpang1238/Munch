fa0e2b962e06cb598bce34fa894ff79dfa68816a
keithpang1238 <50796701+keithpang1238@users.noreply.github.com>
2021-11-17 15:15:26 +1100

    Implement Firebase login

---
+++ app/src/main/java/com/example/munch/LoginActivity.java
@@ -0,0 +1,79 @@
+package com.example.munch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button loginBtn;
+    private EditText usernameEdit;
+    private EditText passwordEdit;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginBtn = findViewById(R.id.loginBtn);
+        usernameEdit = findViewById(R.id.usernameEdit);
+        passwordEdit = findViewById(R.id.passwordEdit);
+
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.loginBtn:
+                verifyCredentials();
+            default:
+                break;
+        }
+    }
+
+    private void verifyCredentials() {
+        String email = usernameEdit.getText().toString().trim();
+        String password = passwordEdit.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            usernameEdit.setError("Email is required");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            usernameEdit.setError("Password is required");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+            new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    } else {
+                        Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        );
+    }
+
+
+}
